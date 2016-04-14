f6818c1d5de9f4d42e1c3be8b5b3ddd24213ac16
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-04-14 10:17:12 -0400

    Add JavaScript and Remove Document File Type

---
+++ app/assets/javascripts/controllers/documentsController.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+//   $('form.new_document').on('submit', app.documents.controllers.create.init)
+// })
+//
+// app.documents.controllers = {
+//   create: {
+//     init: function(event) {
+//       event.preventDefault();
+//       // var title = $('#document_title').val();
+//       // var text = $('#document_text').val();
+//       var action = $('form#new_document').attr('action');
+//       // var document = { title: title, text: text }
+//       var information = $('form#new_document').serializeArray();
+//
+//
+//       $.ajax({
+//         url: action,
+//         method: 'POST',
+//         data: information
+//       })
+//     }
+//   }
+// }
