9ad5f6ad01c76b238114574fb2668c85616c74c2
mindyyuan <mindyuan@gmail.com>
2016-04-15 08:34:20 -0400

    added exclamation

--- app/assets/javascripts/controllers/documentsController.js
+++ app/assets/javascripts/controllers/documentsController.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   //   $('.forms').toggle();
   // })
   function hideSentiment() {
-    $('#sentiment_analysis').empty(); 
-    $('#document_form').show();   
+    $('#sentiment_analysis').empty();
+    $('#document_form').show();
   }
     $('body').on('click', '#populate_doc_form', hideSentiment);
     $('#analyze').on('click', hideSentiment);
@@ -17,8 +17,8 @@ app.documents.controllers = {
   create: {
     init: function(event) {
       event.preventDefault();
-      $('#sentiment_analysis').empty(); 
-      $('#document_form').show();                   
+      $('#sentiment_analysis').empty();
+      $('#document_form').show();
       var action = $('form#new_document').attr('action');
       var information = $('form#new_document').serializeArray();
       $.ajax({
@@ -29,14 +29,14 @@ app.documents.controllers = {
         var stuff = `<p>
           <ul>
             <li>Sentiment:` + data.sentiment + `</li>
-            <li>Sentiment:` + data.polarity_score + `</li>
-            <li>Sentiment:` + data.sentiment_percentage + `%</li>
+            <li>Polarity:` + data.polarity_score + `</li>
+            <li>Sentiment Percentage:` + data.sentiment_percentage + `%</li>
           </ul>
           <a class="btn btn-primary" href="`+ data.document_path +`" role="button">See Full Analysis</a>
-          <div class="btn btn-primary" id="populate_doc_form" role="button">Analyze New Document</div>          
+          <div class="btn btn-primary" id="populate_doc_form" role="button">Analyze New Document</div>
         </p>
         `
-        $('#document_form').hide();        
+        $('#document_form').hide();
         $('#sentiment_analysis').append(stuff);
       })
     }
--- app/assets/javascripts/controllers/twitterSearchesController.js
+++ app/assets/javascripts/controllers/twitterSearchesController.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   function resetForm() {
-    $('#twitter_sentiment_analysis').empty(); 
-    $('#twitter_form').show();   
-    $('#twitter_search_search_query').val('');     
-    $('#twitter_search_description').val('');     
+    $('#twitter_sentiment_analysis').empty();
+    $('#twitter_form').show();
+    $('#twitter_search_search_query').val('');
+    $('#twitter_search_description').val('');
   }
     $('body').on('click', '#populate_twitter_form', resetForm);
     $('#analyze').on('click', resetForm);
@@ -16,8 +16,8 @@ app.twitterSearches.controllers = {
   create: {
     init: function(event) {
       event.preventDefault();
-      $('#twitter_sentiment_analysis').empty(); 
-      $('#twitter_form').show();   
+      $('#twitter_sentiment_analysis').empty();
+      $('#twitter_form').show();
       var action = $('form#new_twitter_search').attr('action');
       var information = $('form#new_twitter_search').serializeArray();
       $.ajax({
@@ -27,14 +27,14 @@ app.twitterSearches.controllers = {
       }).success(function(data) {
         var stuff = `<p>
           <ul>
-            <li>Sentiment Percentage:` + data.average_sentiment + `%</li>
             <li>Sentiment:` + data.display_sentiment + `</li>
+            <li>Sentiment Percentage:` + data.average_sentiment + `%</li>
           </ul>
           <a class="btn btn-primary" href="`+ data.twitter_search_path +`" role="button">See Full Analysis</a>
-          <div class="btn btn-primary" id="populate_twitter_form" role="button">Analyze New Twitter Search</div>          
+          <div class="btn btn-primary" id="populate_twitter_form" role="button">Analyze New Twitter Search</div>
         </p>
         `
-        $('#twitter_form').hide();        
+        $('#twitter_form').hide();
         $('#twitter_sentiment_analysis').append(stuff);
       })
     }
